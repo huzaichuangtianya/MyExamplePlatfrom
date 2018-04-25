7ef3effe9d098e4520a744ca09713f659dcabb09
quliang <huzaichuangtianya@163.com>
2018-04-25 17:46:41 +0800

    modify

--- app/src/main/java/com/example/quliang/myapplication/ui/Main8Activity.java
+++ app/src/main/java/com/example/quliang/myapplication/ui/Main8Activity.java
@@ -1,10 +1,16 @@
 package com.example.quliang.myapplication.ui;
 
+import android.app.Instrumentation;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.TextView;
 
 import com.example.quliang.myapplication.R;
 import com.example.quliang.myapplication.adapter.RecyclerViewSimpleAdapter;
@@ -12,12 +18,24 @@ import com.example.quliang.myapplication.bean.SimpleBean;
 
 public class Main8Activity extends AppCompatActivity implements View.OnClickListener {
 
+    RecyclerView recyclerView;
+    TextView     xf;
+    Handler handler=new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what){
+                case 1:
+                    break;
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main8);
-        RecyclerView recyclerView   = (RecyclerView) findViewById(R.id.recyclerView);
+         recyclerView   = (RecyclerView) findViewById(R.id.recyclerView);
+         xf   = (TextView) findViewById(R.id.xf);
 
         RecyclerViewSimpleAdapter recyclerViewSimpleAdapter=new RecyclerViewSimpleAdapter();
         for (int i=0;i<30;i++){
@@ -25,10 +43,31 @@ public class Main8Activity extends AppCompatActivity implements View.OnClickList
         }
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(recyclerViewSimpleAdapter);
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
 
+                MotionEvent evenDownt = MotionEvent.obtain(System.currentTimeMillis(),
+                        System.currentTimeMillis() + 100, MotionEvent.ACTION_DOWN, x, y, 0);
+                dispatchTouchEvent(evenDownt);
+
+                MotionEvent eventMv1 = MotionEvent.obtain(System.currentTimeMillis(),
+                        System.currentTimeMillis() + 100, MotionEvent.ACTION_MOVE, x, y-100, 0);
+                dispatchTouchEvent(eventMv1);
+
+                MotionEvent eventUp = MotionEvent.obtain(System.currentTimeMillis(),
+                        System.currentTimeMillis() + 100, MotionEvent.ACTION_UP, x, y, 0);
+                dispatchTouchEvent(eventUp);
+
+                evenDownt.recycle();
+                eventMv1.recycle();
+                eventUp.recycle();
+
+            }
+        },3000);
     }
 
-
+int x=10,y=1721;
     @Override
     public void onClick(View v) {
 
---
+++ app/src/main/java/com/example/quliang/myapplication/ui/Main9Activity.java
@@ -0,0 +1,68 @@
+package com.example.quliang.myapplication.ui;
+
+import android.app.Instrumentation;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.os.SystemClock;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.quliang.myapplication.R;
+import com.example.quliang.myapplication.adapter.RecyclerViewSimpleAdapter;
+import com.example.quliang.myapplication.bean.SimpleBean;
+
+public class Main9Activity extends AppCompatActivity implements View.OnClickListener {
+final static String TAG=Main9Activity.class.getSimpleName();
+    TextView     text;
+    Handler handler=new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what){
+                case 1:
+                    break;
+            }
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main9);
+
+        text   = (TextView) findViewById(R.id.text);
+        text.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(Main9Activity.this,"kankan",Toast.LENGTH_LONG).show();
+            }
+        });
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+            }
+        },3000);
+    }
+
+    private int x,y=1721;
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        Log.d(TAG,"x:"+event.getX());
+        Log.d(TAG,"y:"+event.getY());
+        return super.onTouchEvent(event);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
